486e326f7b3e637db0e85c582a2327fc3e254d7a
Alexander Jakobsson <jakobsson.alex@gmail.com>
2017-11-27 13:00:51 +0100

    Done. Version 1.0.

--- code.js
+++ code.js
@@ -1,6 +1,34 @@
-function renderMovie(movie) {
+function renderMovie(Movie) {
     // Rendering Logic will go here 
-    document.getElementByTag("body");
+    document.querySelector("body div").innerHTML = 
+        "<div id=\"BigBoy\">" 
+        + "<div id=\"ThePic\">"
+        + "<img src=\"" + Movie.ImageURL + "\" />"
+        + "<p>" + Movie.ImageSubHeading + "</p>"
+        + "</div>"
+        + "<div id=\"TheKid\">"
+        + "<h1>" + Movie.MovieTitle + "</h1>"
+        + "<h2>" + Movie.Review + "</h2>"
+        + "<h3>" + Movie.ActorsSuperHeading + "</h3>"
+        + "<ul>" + ActorString + "</ul>"
+        + "</div>"
+        + "</div>";
 
-
+    /*
+    <div id="BigBoy">
+        <div id="ThePic">
+            <img>
+            <p></p> 
+        </div>
+        <div id="TheKid">
+            <h1></h1>
+            <h2></h2>
+            <h3></h3>
+            <ul>
+            </ul>
+        </div>
+    </div>
+    */
 }
+
+renderMovie(MovieData);
--- data.js
+++ data.js
@@ -1,10 +1,16 @@
 let MovieData = {
-    Image: "https://static.rogerebert.com/uploads/movie/movie_poster/dark-city-1998/large_5EzPQs7yDboIVZJa7Feaw7SioZa.jpg",
+    ImageURL: "https://static.rogerebert.com/uploads/movie/movie_poster/dark-city-1998/large_5EzPQs7yDboIVZJa7Feaw7SioZa.jpg",
     ImageSubHeading: "-> --> |>0<| <-- <-",
     WebsiteTitle: "Film Review: Dark City",
     MovieTitle: "Film Review |> Dark City |>",
     Review: "This movie is SICK. It is better than 'The Matrix'. I enjoyed watching it. No shit.",
     ActorsSuperHeading: "Starring:",
     Actors: ["Rufus Sewell", "William Hurt", "Kiefer Sutherland", "Jennifer Connoly"],
+    //ActorString: Actors.forEach(makeStringOfArray(ActorString, Actor)),
 }
 
+let ActorString = "";
+
+for (i = 0; i < MovieData.Actors.length; i++) {
+    ActorString += "<li>" + MovieData.Actors[i] + "</li>";
+}
