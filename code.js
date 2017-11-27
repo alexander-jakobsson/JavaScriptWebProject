2fafb484bf40434846b1ad70b7f7a1b766f032f7
Alexander Jakobsson <jakobsson.alex@gmail.com>
2017-11-27 15:00:19 +0100

    Add .gitignore

--- code.js
+++ code.js
@@ -1,5 +1,4 @@
 function renderMovie(Movie) {
-    // Rendering Logic will go here 
     document.querySelector("body div").innerHTML = 
         "<div id=\"BigBoy\">" 
         + "<div id=\"ThePic\">"
@@ -9,54 +8,34 @@ function renderMovie(Movie) {
         + "<div id=\"TheKid\">"
         + "<h1>" + Movie.MovieTitle + "</h1>"
         + "<div id=\"stars\">" +
-            "<span class=\"Star\">&#9733</span>" +
-            "<span class=\"emptyStar\">&#9733</span>" +
-            "<span class=\"emptyStar\">&#9733</span>" +
-            "<span class=\"emptyStar\">&#9733</span>" +
-            "<span class=\"emptyStar\">&#9733</span>" 
+            "<span >&#9733</span>" +
+            "<span >&#9733</span>" +
+            "<span >&#9733</span>" +
+            "<span >&#9733</span>" +
+            "<span >&#9733</span>" 
         + "</div>"
         + "<h2>" + Movie.Review + "</h2>"
         + "<h3>" + Movie.ActorsSuperHeading + "</h3>"
         + "<ul>" + ActorString + "</ul>"
         + "</div>"
         + "</div>";
-
-    /*
-    <div id="BigBoy">
-        <div id="ThePic">
-            <img>
-            <p></p> 
-        </div>
-        <div id="TheKid">
-            <h1></h1> // Title
-            <div><span></div>// Stars
-            <h2></h2>
-            <h3></h3>
-            <ul>
-            </ul>
-        </div>
-    </div>
-    */
 }
 
 renderMovie(MovieData);
 var StarButtonRow = document.querySelectorAll("#stars>span");
+
 function changeStarRating(grade){
-    // Update stars so they appear correctly according to given grade!
-
-    for (let i = 0; i <= grade; i++) {
-        StarButtonRow[i].classList.add("filledStar");
-        StarButtonRow[i].classList.remove("emptyStar");
-        
-    }
-
-    for (let j = 0; j <= StarButtonRow.length - 1; j++) {
-        if (j > grade) {
-            StarButtonRow[j].classList.add("emptyStar");
-            StarButtonRow[j].classList.remove("filledStar");
+  
+    for (let i = 0; i <= StarButtonRow.length - 1; i++) {
+        console.log("Grade: " + grade + " Index: " + i);
+        if (i <= grade) {  
+            console.log("Added filledStar to index: " + i);    
+            StarButtonRow[i].classList.add("filledStar");
+        } else {
+            console.log("Removed filledStar from index: " + i);
+        StarButtonRow[i].classList.remove("filledStar");
         }
     }
-
     
 
     console.log("I'm in here!");
