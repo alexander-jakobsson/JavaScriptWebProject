47f145503c643423ab7cc1ce9728e351447b2347
Alexander Jakobsson <jakobsson.alex@gmail.com>
2017-11-29 13:32:56 +0100

    Try again!

--- data.ts
+++ data.ts
@@ -2,9 +2,9 @@ export const MovieData = {
     Actors: ["Rufus Sewell", "William Hurt", "Kiefer Sutherland", "Jennifer Connoly"],
     ActorsSuperHeading: "Starring:",
     ImageSubHeading: "-> --> |>0<| <-- <-",
-    MovieTitle: "|>Film Review_ |>Dark City_",
-    Review: "This movie is SICK. It is better than \'The Matrix\'. I enjoyed watching it. No shit.",
     ImageURL:
         "https://static.rogerebert.com/uploads/movie/movie_poster/dark-city-1998/large_5EzPQs7yDboIVZJa7Feaw7SioZa.jpg",
+    MovieTitle: "|>Film Review_ |>Dark City_",
+    Review: "This movie is SICK. It is better than \'The Matrix\'. I enjoyed watching it. No shit.",
     WebsiteTitle: "Film Review: Dark City",
 };
